c934cbc58058146d8ccd57cd911cca28e4177377
Jonas Bockstal <jonas.bockstal@ond.vlaanderen.be>
2023-03-13 21:15:00 +0100

    Init project

---
+++ src/main/java/dev/jonasbockstal/hibernatelazyinitializationexceptiondemo/HibernateLazyinitializationexceptionDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.jonasbockstal.hibernatelazyinitializationexceptiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLazyinitializationexceptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateLazyinitializationexceptionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jonasbockstal/hibernatelazyinitializationexceptiondemo/HibernateLazyinitializationexceptionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jonasbockstal.hibernatelazyinitializationexceptiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateLazyinitializationexceptionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
